a6c5340663b4df2e541854d06a06971a11fd930a
DevSilv <38167818+DevSilv@users.noreply.github.com>
2020-07-16 14:06:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Recursion algorithms
+
+## License
+
+This project has currently **no license**.
+
+For details what does "no license" mean, see [this GitHub guide on "no license"](https://choosealicense.com/no-permission/). A quote from that site:
+
+> When you make a creative work (which includes code), the work is under exclusive copyright by default. Unless you include a license that specifies otherwise, nobody else can use, copy, distribute, or modify your work without being at risk of take-downs, shake-downs, or litigation. Once the work has other contributors (each a copyright holder), “nobody” starts including you.
+
+## Disclaimers
+
+**The code that this project contains is an example code that is not intended to be run.** Its purpose is only to exemplify implementations of algorithms. While probably nothing dangerous would happen, you may run it only under your own responsibility.
+
+**Pseudocode and any other form of algorithms that this project contains are examples, and they are not intended to be implemented.** Their purpose is only to exemplify my understanding of said algorithms. They are **not** to be treated as correct.
+
+The code may contain bugs.
+
+## Notes for correctness of algorithms and their implementations
+
+If the pseudocode of an algorithm and any of the implementations of this algorithm differ in any way, neither of them has preference in terms of correctness.
+
+## Links
+
+### JS
+
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+- https://jsdoc.app/tags-callback.html
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
---
+++ js/recursion-benchmarks.js
@@ -0,0 +1,155 @@
+const process = require("process");
+const {
+	shallowAreArraysEqual,
+	deepAreArraysEqual,
+	split,
+	slice,
+	findFirst,
+	findAll,
+	reduce,
+	map,
+	getLength,
+	countIf,
+	splitIf,
+	join
+} = require("./index");
+
+/**
+ * @param {number} start
+ * @param {number} end
+ * @returns {number[]}
+ */
+const getIntegerRange
+	= (start, end) =>
+		Array
+			.from(
+				new Array(end + 1)
+					.keys()
+			)
+			.filter(elem => elem >= start);
+
+/**
+ * @callback GetMeanExecTimeFn
+ * @returns {number[]}
+ */
+
+/**
+ * @param {GetMeanExecTimeFn} fn
+ * @param {number} count
+ * @returns {number[]}
+ */
+const getMeanExecTime
+	= (fn, count, meanS, meanNs, counter) =>
+		counter === count
+			? [
+				meanS,
+				meanNs
+			]
+			: (() => {
+				const times = fn();
+
+				return getMeanExecTime(
+					fn,
+					count,
+					(meanS + times[0]) / 2,
+					(meanNs + times[1]) / 2,
+					counter + 1
+				);
+			})();
+
+const benchmark
+	= (benchmarkedFnName, fn, repeatCount) => {
+		const getExecTime
+			= () => {
+				const start = process.hrtime();
+				fn();
+				const end = process.hrtime(start);
+
+				return end;
+			};
+		const meanExecTime
+			= getMeanExecTime(getExecTime, repeatCount, 0.0, 0.0, 0);
+
+		return {
+			outputResult:
+				() => {
+					process.stdout.write(`${benchmarkedFnName}\n`);
+					process.stdout.write(`${meanExecTime[0]}s\n`);
+					process.stdout.write(`${meanExecTime[1] / 1000000}ms\n\n`);
+				}
+		};
+	};
+
+const integerRange = getIntegerRange(1000, 2000);
+
+benchmark(
+	"shallowAreArraysEqual",
+	() => shallowAreArraysEqual(integerRange, integerRange),
+	100
+).outputResult();
+
+benchmark(
+	"deepAreArraysEqual",
+	() => deepAreArraysEqual(integerRange, integerRange),
+	100
+).outputResult();
+
+benchmark(
+	"split",
+	() => split(integerRange, 100, []),
+	100
+).outputResult();
+
+benchmark(
+	"slice",
+	() => slice(integerRange, 400, 600, [], 0),
+	100
+).outputResult();
+
+benchmark(
+	"findFirst",
+	() => findFirst(integerRange, elem => elem > 1500),
+	100
+).outputResult();
+
+benchmark(
+	"findAll",
+	() => findAll(integerRange, elem => elem > 1500, []),
+	100
+).outputResult();
+
+benchmark(
+	"reduce",
+	() => reduce(integerRange, (res, cur) => res / cur, 1.0),
+	100
+).outputResult();
+
+benchmark(
+	"map",
+	() => map(integerRange, elem => elem ** 2, []),
+	100
+);
+
+benchmark(
+	"getLength",
+	() => getLength(integerRange, 0),
+	100
+);
+
+benchmark(
+	"countIf",
+	() => countIf(integerRange, elem => elem % 3 === 2, 0),
+	100
+).outputResult();
+
+benchmark(
+	"splitIf",
+	() => splitIf(integerRange, elem => elem % 30 === 17, [], []),
+	100
+).outputResult();
+
+benchmark(
+	"join",
+	() => join(integerRange, integerRange),
+	100
+).outputResult();
