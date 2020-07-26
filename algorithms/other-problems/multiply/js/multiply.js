70c5588d29451f4c445b90628c3374db6152ae4a
DevSilv <38167818+DevSilv@users.noreply.github.com>
2020-07-26 16:56:17 -0400

    Separate the pseudocode of the multiply algorithm

--- algorithms/other-problems/multiply/js/multiply.js
+++ algorithms/other-problems/multiply/js/multiply.js
@@ -1,13 +1,4 @@
 /**
- * 	MULTIPLY (num1, num2)
- * 		if equal(num2, 0)
- * 		then return 0
- * 		else return num1 + MULTIPLY(num1, subtract(num2, 1))
- * 
- * 	MULTIPLY(4, 5)
- */
-
-/**
  * @param {number} num1 A real number
  * @param {number} num2 An integer >= 0
  * @returns {number} A real number >= 0
