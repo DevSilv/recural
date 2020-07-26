d4c96364650867b0a2840bc81866b3637cd37078
DevSilv <38167818+DevSilv@users.noreply.github.com>
2020-07-26 14:07:26 -0400

    Separate the pseudocode of the count-if algorithm

--- algorithms/other-problems/count-if/js/count-if.js
+++ algorithms/other-problems/count-if/js/count-if.js
@@ -1,16 +1,4 @@
 /**
- * 	COUNT_IF (arr, pred, res)
- * 		if empty(arr)
- * 		then return res
- * 		else
- * 			if pred(arr[0])
- * 			then return COUNT_IF(slice(arr, 1, len(arr)), pred, res + 1)
- * 			else return COUNT_IF(slice(arr, 1, len(arr)), pred, res)
- * 
- * 	COUNT_IF ([1, 2, 3], elem -> equal(remainder(elem, 2), 0), 0)
- */
-
-/**
  * @callback CountIfFn
  * @param {any} elem
  * @returns {boolean}
