45f7cbe1dd17c2f45ef7ac7ef05968f5228bd92b
DevSilv <38167818+DevSilv@users.noreply.github.com>
2020-07-27 12:50:13 -0400

    Separate the pseudocode of the find-all algorithm

--- algorithms/searching/find-all/js/find-all.js
+++ algorithms/searching/find-all/js/find-all.js
@@ -1,16 +1,4 @@
 /**
- * 	FIND_ALL (arr, pred, res)
- * 		if empty(arr)
- * 		then return res
- * 		else
- * 			if pred(arr[0])
- * 			then return FIND_ALL(slice(arr, 1, len(arr)), pred, concat(res, arr[0]))
- * 			else return FIND_ALL(slice(arr, 1, len(arr)), pred, res)
- * 
- * 	FIND_ALL([1, 2, 3, 4], elem -> equal(remainder(elem, 2), 0), [])
- */
-
-/**
  * @callback FindAllPredicate
  * @param {any} elem
  * @returns {boolean}
