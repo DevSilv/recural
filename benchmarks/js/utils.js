c32e0dbca1cccbbfece5ef5133074aeb3f019675
DevSilv <38167818+DevSilv@users.noreply.github.com>
2020-07-27 19:26:11 -0400

    Fix test cases for the generate-random-permutation algorithm
    Add a utility function that shall have been added together with the generate-random-permutation algorithm

---
+++ benchmarks/js/utils.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} start Start of the range (inclusive); an integer
+ * @param {number} end End of the range (inclusive); an integer
+ * @returns {number[]} The generated range
+ */
+exports.generateIntegerRange
+	= (start, end) =>
+		Array
+			.from(
+				new Array(end + 1)
+					.keys()
+			)
+			.filter(elem => elem >= start);
