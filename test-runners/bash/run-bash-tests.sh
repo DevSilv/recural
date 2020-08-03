3dcb4bd3e4426a499718cea6d78a64abf78149b6
DevSilv <38167818+DevSilv@users.noreply.github.com>
2020-08-03 14:08:47 -0400

    Remove unnecessary code

--- test-runners/bash/run-bash-tests.sh
+++ test-runners/bash/run-bash-tests.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Uncomment to debug
-set -x
+# set -x
 
 scriptPath="${0}"
 scriptDirPath="$(dirname ${scriptPath})"
 testFiles="$(find ${scriptDirPath}/../../algorithms/ -type f -name '*-tests.sh')"
 
-echo "B"
+# echo "B"
 for file in ${testFiles}
 do
     bash "${file}"
 
-    echo "A"
+    # echo "A"
 
     if [[ "${?}" != 0 ]]
     then exit 1
     fi
 done
-echo "C"
+# echo "C"
