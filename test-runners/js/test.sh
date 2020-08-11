bbd7503babf251a60b55a7d2021e693928d4f128
DevSilv <38167818+DevSilv@users.noreply.github.com>
2020-08-11 11:40:12 -0400

    Make testing progress be displayed

--- test-runners/js/test.sh
+++ test-runners/js/test.sh
@@ -6,6 +6,12 @@
 scriptPath="${0}"
 scriptDirPath="$(dirname ${scriptPath})"
 testFiles="$(find ${scriptDirPath}/../../algorithms/ -type f -name '*-tests.js')"
+testFilesCount="$( echo ${testFiles} | wc -w )"
+count=0
+percent=0
+cur=10
+
+printf "[ "
 
 for file in ${testFiles}
 do
@@ -14,4 +20,15 @@ do
     if [[ "${?}" != 0 ]]
     then exit 1
     fi
+
+    count="$(( count + 1 ))"
+    percent="$(( ${count} * 100 / ${testFilesCount} ))"
+    
+    while [[ "$(( ${cur} - ${percent} ))" -le 0 ]]
+    do
+        printf "${cur}%% "
+        cur="$(( ${cur} + 10 ))"
+    done
 done
+
+printf "]\n"
