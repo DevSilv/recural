9bd438a0ba9bacc8d906aee587be0c77da313f7a
DevSilv <38167818+DevSilv@users.noreply.github.com>
2021-02-20 18:07:44 -0500

    Document Clojure code

--- algorithms/string-analysing/transpiling/clojure/clojureLikeToJavaScript.clj
+++ algorithms/string-analysing/transpiling/clojure/clojureLikeToJavaScript.clj
@@ -2,7 +2,17 @@
 
 (declare parse)
 
-(defn parseFunctionCall [arr part res]
+(defn parseFunctionCall
+    "@param arr Characters representing the function call code to parse.
+    @param part A string of characters; always an empty string; an auxiliary
+        variable; between consecutive recursive invokations of the function,
+        it represents an identifier currently being parsed.
+    @param res A vector; always an empty vector; an auxiliary variable;
+        between consecutive recursive invokations of the function, it represents
+        the result of parsing the characters in arr computed so far.
+    @returns nil if arr contains an invalid character; otherwise, the result
+        of parsing the characters in arr."
+    [arr part res]
     (if (or (= (count arr) 0) (= (first arr) ")"))
         (parse arr (conj (rest res) (conj (first res) part)))
         (if (re-matches #"^[a-zA-Z]+$" (first arr))
@@ -14,7 +24,14 @@
                     (conj (rest res) (conj (first res) part)))
                 nil))))
 
-(defn parse [arr res]
+(defn parse
+    "@param arr Characters representing the code to parse.
+    @param res A vector; always an empty vector; an auxiliary variable;
+        between consecutive recursive invokations of the function, it represents
+        the result of parsing the characters in arr computed so far.
+    @returns nil if arr contains an invalid character; otherwise, the result
+        of parsing the characters in arr."
+    [arr res]
     (if (= (count arr) 0)
         res
         (if (= (first arr) "(")
@@ -23,7 +40,14 @@
                 (parse (rest arr) res)
                 nil))))
 
-(defn transpileHelper [arr res]
+(defn transpileHelper
+    "@param arr The result of parsing code.
+    @param res A string of characters; always an empty string; an auxiliary
+        variable; between consecutive recursive invokations of the function,
+        it represents the code transpiled from the content of arr so far.
+    @returns nil if arr is nil; otherwise, the code transpiled from the content
+        of arr."
+    [arr res]
     (if (= arr nil)
         arr
         (if (= (count arr) 0)
@@ -37,5 +61,10 @@
                     (clojure.string/join "," (rest (first arr)))
                     ");")))))
 
-(defn transpile [s]
+(defn transpile
+    "@param s A string of characters; the code to transpile.
+    @returns nil or a string of characters; nil if s does not represent valid
+        code; otherwise, the the code transpiled from the code represented
+        by s."
+    [s]
     (transpileHelper (parse (clojure.string/split s #"") []) ""))
--- test-runners/clojure/test.clj
+++ test-runners/clojure/test.clj
@@ -1,8 +1,20 @@
-(defn tstHelper [fs cur]
+(defn tstHelper
+    "@param fs Functions, each of which represents a tests, takes no argument,
+        and returns true if the test passes and false if it does not.
+    @param cur An integer number from 0 (including) to inifinity; always 0;
+        an auxiliary variable; between consecutive recursive invokations
+        of the function, it represents the number of the test being currently
+        checked for passing.
+    @sideeffects Prints to the output stream."
+    [fs cur]
     (if (not (= cur (count fs)))
         (if (= ((nth fs cur)) false)
             (println "At least one test failed:" cur)
             (tstHelper fs (inc cur)))))
 
-(defn tst [fs]
+(defn tst
+    "@param fs Functions, each of which represents a tests, takes no arument,
+        and returns true if the test passes and false if it does not.
+    @sideeffects Prints to the output stream."
+    [fs]
     (tstHelper fs 0))
